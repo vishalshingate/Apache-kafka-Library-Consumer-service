3f80f094ee33151e7894094e089c5c0e7e6e0fb7
vishalshingate <vishalshingate26@gmail.com>
2025-12-04 00:21:28 +0530

    Created project template

---
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.learnkafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+// for older version of kafka @EnableKafka
+public class LibraryEventsConsumerConfig {
+}
