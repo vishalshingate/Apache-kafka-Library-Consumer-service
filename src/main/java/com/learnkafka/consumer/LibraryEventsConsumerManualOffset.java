6aa9743f1afd0b7e6b3e5117a7108836cea99ab3
vishalshingate <vishalshingate26@gmail.com>
2025-12-08 19:13:57 +0530

    Added the manual commit offset teq

---
+++ src/main/java/com/learnkafka/consumer/LibraryEventsConsumerManualOffset.java
@@ -0,0 +1,29 @@
+package com.learnkafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.AcknowledgingMessageListener;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventsConsumerManualOffset implements AcknowledgingMessageListener<Integer, String> {
+
+    /**
+     * Listens to messages from the "library-events" this is simple consumer method which has the poll call
+     * it will poll multiple records but it will pass one by one to this method
+     * @param consumerRecord  this will be the type of ConsumerRecords<?, ?>
+     */
+
+    @Override
+    @KafkaListener(topics = {"library-events"})
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord, Acknowledgment acknowledgment) {
+        log.info("Message received: {}", consumerRecord);
+        //manual offset commit
+        acknowledgment.acknowledge();
+    }
+}
