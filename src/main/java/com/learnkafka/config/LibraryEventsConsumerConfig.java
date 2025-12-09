6bf1f267abf55c5d3bb47ba76994e01fe7fbc2f8
vishalshingate <vishalshingate26@gmail.com>
2025-12-09 11:48:52 +0530

    Added code for Recovery having recoverable topic and dlq for discarding other exceptions

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,19 +1,24 @@
 package com.learnkafka.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.ContainerCustomizer;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
@@ -25,17 +30,50 @@ import java.util.List;
 public class LibraryEventsConsumerConfig {
 
     private final KafkaProperties properties;
+    private final KafkaTemplate<?, ?> kafkaTemplate;
+
+    @Value("${topics.retry}")
+    private String retryTopic;
+
+    @Value("${topics.dlq}")
+    private String dlqTopic;
 
 
-    public LibraryEventsConsumerConfig(KafkaProperties properties) {
+    public LibraryEventsConsumerConfig(KafkaProperties properties, KafkaTemplate<?, ?> kafkaTemplate) {
         this.properties = properties;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    /**
+     * Custom DeadLetterPublishingRecoverer which will send the messages to retry topic or dlq topic based on exception type
+     * @return DeadLetterPublishingRecoverer
+     */
+    public DeadLetterPublishingRecoverer publishingRecoverer() {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate,
+            (r, e) -> {
+                if(e instanceof RecoverableDataAccessException ) {
+                    return  new TopicPartition(retryTopic, r.partition());
+                } else {
+                    return new TopicPartition(dlqTopic, r.partition());
+                }
+            });
+
+        return recoverer;
     }
 
     public DefaultErrorHandler errorHandler() {
         //custom error handler logic
         var fixedBackOff= new FixedBackOff(1000L, 2L); // retry every 1 second, max 2 retries
+        /**
+         * this is custom error handler which will handle the exceptions thrown by the listener
+         * we can configure the retry logic here
+         * we can configure the dead letter topic here or Recovery logic here
+         */
+        var errorHandler = new DefaultErrorHandler(
+            publishingRecoverer(),
+            fixedBackOff
+        );
 
-        var errorHandler = new DefaultErrorHandler(fixedBackOff);
         var exceptionsToIgnoreList= List.of(
             IllegalArgumentException.class
         );
@@ -52,8 +90,9 @@ public class LibraryEventsConsumerConfig {
     }
 
 
+
     @Bean
-    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+    public ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
         ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
         ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory,
         ObjectProvider<ContainerCustomizer<Object, Object, ConcurrentMessageListenerContainer<Object, Object>>> kafkaContainerCustomizer) {
